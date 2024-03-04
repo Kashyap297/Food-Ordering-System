d5584ec6a2ec2a0369be8787fe3eb3a94aafd60a
Kashyap297 <kashyap29700@gmail.com>
2024-03-04 09:02:20 +0530

    banner img done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,10 @@ const Home = () => {
     return (
         <>
             <div className="banner pt-3 ">
-                <h1 className='text-center mt-3'>Welcome to Home Page</h1>
+                {/* <h1 className='text-center mt-3'>Welcome to Home Page</h1> */}
+            </div>
+            <div className="banner-img col-12">
+                <img src="https://i.pinimg.com/originals/fc/a3/44/fca344f23100b1152a8476616aa7d777.jpg" alt="" />
             </div>
         </>
     )
